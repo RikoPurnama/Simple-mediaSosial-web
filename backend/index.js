7feed2a026c7235ea99d52c89e4057ee74ae14f7
RikoPurnama <rikopurnama9051@gmail.com>
2024-06-09 22:47:11 +0700

    test 2

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import connectMonggoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
-import cors from "cors";
-import passport from "passport";
-import cookieSession from "cookie-session";
-import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import { v2 as cloudinary } from "cloudinary";
 
 import authRoutes from "./routes/auth.js";
@@ -30,23 +26,6 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
-app.use(
-  cookieSession({
-    name: "session",
-    keys: ["cyberwolve"],
-    maxAge: 24 * 60 * 60 * 100,
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(
-  cors({
-    origin: "http://localhost:5000",
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true,
-  })
-)
 
 app.get("/", (req, res) => {
   res.send("Server is running");
