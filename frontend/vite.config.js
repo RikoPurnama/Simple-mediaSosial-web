c50c6bb85290697bd9e0647b2409a4a4f463fc91
RikoPurnama <rikopurnama9051@gmail.com>
2024-06-09 22:35:14 +0700

    change endpoint api in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
 			"/api": {
-				target: "http://localhost:5000",
+				target: "https://v-api-ten.vercel.app/",
 				changeOrigin: true,
 			},
 		},
