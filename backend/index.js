695af5fc016802ea08377fe8dd2705f917652d89
RikoPurnama <rikopurnama9051@gmail.com>
2024-06-09 23:17:26 +0700

    test

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
